05a3aadd9649f823e516465684dd0020fb768a6a
HUUPHUNG <21522481@gm.uit.edu.vn>
2024-07-07 18:16:55 +0700

    cập nhật

--- lib/ui/myOrder/my_order.dart
+++ lib/ui/myOrder/my_order.dart
@@ -1,7 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
-import 'components/body.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
--- lib/ui/wishlist/wish_list.dart
+++ lib/ui/wishlist/wish_list.dart
@@ -1,16 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_project/ui/authentication/homepage.dart';
+import 'package:flutter_project/ui/search/ProductDetailPage.dart';
 import 'package:flutter_svg/svg.dart';
 
 class wishList extends StatefulWidget {
-  
   @override
   State<wishList> createState() => _wishListState();
 }
 
 class _wishListState extends State<wishList> {
-
   User? user = FirebaseAuth.instance.currentUser;
 
   late final _wishlistStream = FirebaseFirestore.instance
@@ -35,110 +35,119 @@ class _wishListState extends State<wishList> {
           var docs = snapshot.data!.docs;
           // return Text('${docs.length}');
           return ListView.builder(
-              itemCount: docs.length, itemBuilder: (context, index) {
+              itemCount: docs.length,
+              itemBuilder: (context, index) {
                 return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
-            child: Container(
-              width: 882,
-              height: 158,
-              decoration: BoxDecoration(
-                color: Color(0xffFAF6F6),
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-              child: Row(
-                children: [
-                  Container(
-                    width: 150,
-                    height: 150,
-                    child: ClipRRect(
-                      borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(8.0),
-                        bottomLeft: Radius.circular(8.0),
-                      ),
-
-                      // color: Colors.green,
-                      child: Image.network(
-                        docs[index]['imgURL'], // Đường link ảnh của bạn
-                        fit: BoxFit
-                            .cover, // Điều chỉnh cách ảnh hiển thị trong Container
-                      ),
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 10.0),
+                  child: InkWell(
+                    onTap: () {
+                      // Hàm xử lý logic khi nhấn vào một item
+                      navigateToProductDetailPage(context, docs[index]['imgURL']);
+                    },     
+                  child: Container(
+                    width: 882,
+                    height: 158,
+                    decoration: BoxDecoration(
+                      color: Color(0xffFAF6F6),
+                      borderRadius: BorderRadius.circular(8.0),
                     ),
-                  ),
-                  Container(
-                    width: 350,
-                    height: 150,
-                    child: Column(
+                    child: Row(
                       children: [
                         Container(
+                          width: 150,
+                          height: 150,
+                          child: ClipRRect(
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(8.0),
+                              bottomLeft: Radius.circular(8.0),
+                            ),
+
+                            // color: Colors.green,
+                            child: Image.network(
+                              docs[index]['imgURL'], // Đường link ảnh của bạn
+                              fit: BoxFit
+                                  .cover, // Điều chỉnh cách ảnh hiển thị trong Container
+                            ),
+                          ),
+                        ),
+                        Container(
                           width: 350,
-                          height: 110,
-                          //! color: Colors.blue[200],
+                          height: 150,
                           child: Column(
                             children: [
-                              Padding(
-                                padding:
-                                  const  EdgeInsets.only(left: 10.0, top: 10.0),
-                                child: Align(
-                                  alignment: Alignment
-                                      .bottomLeft, // Căn chỉnh văn bản
-                                  child: Text(
-                                    docs[index]['title'],
-                                    style: const TextStyle(
-                                        color:
-                                            Colors.black, // Đổi màu văn bản
-                                        fontSize: 20.0,
-                                        fontWeight: FontWeight
-                                            .w500 // Kích thước văn đậm văn bản
+                              Container(
+                                width: 350,
+                                height: 110,
+                                //! color: Colors.blue[200],
+                                child: Column(
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                          left: 10.0, top: 10.0),
+                                      child: Align(
+                                        alignment: Alignment
+                                            .bottomLeft, // Căn chỉnh văn bản
+                                        child: Text(
+                                          docs[index]['title'],
+                                          style: const TextStyle(
+                                              color: Colors
+                                                  .black, // Đổi màu văn bản
+                                              fontSize: 20.0,
+                                              fontWeight: FontWeight
+                                                  .w500 // Kích thước văn đậm văn bản
+                                              ),
                                         ),
-                                  ),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding:
+                                          const EdgeInsets.only(left: 10.0),
+                                      child: Align(
+                                        alignment: Alignment
+                                            .bottomLeft, // Căn chỉnh văn bản
+                                        child: Text(
+                                          docs[index]['description'],
+                                        ),
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ),
                               Padding(
-                                padding: const EdgeInsets.only(left: 10.0),
+                                padding:
+                                    const EdgeInsets.only(left: 10.0, top: 5.0),
                                 child: Align(
-                                  alignment: Alignment
-                                      .bottomLeft, // Căn chỉnh văn bản
+                                  alignment:
+                                      Alignment.bottomLeft, // Căn chỉnh văn bản
                                   child: Text(
-                                    docs[index]['description'],
+                                    docs[index]['price'],
+                                    style: const TextStyle(
+                                      color: Colors.black, // Đổi màu văn bản
+                                      fontSize: 20.0, // Kích thước văn bản
+                                      fontWeight:
+                                          FontWeight.bold, // Độ đậm văn bản
+                                    ),
                                   ),
                                 ),
                               ),
                             ],
                           ),
                         ),
-                        Padding(
-                          padding:
-                              const EdgeInsets.only(left: 10.0, top: 5.0),
-                          child: Align(
-                            alignment:
-                                Alignment.bottomLeft, // Căn chỉnh văn bản
-                            child: Text(
-                              docs[index]['price'],
-                              style: const TextStyle(
-                                color: Colors.black, // Đổi màu văn bản
-                                fontSize: 20.0, // Kích thước văn bản
-                                fontWeight: FontWeight.bold, // Độ đậm văn bản
-                              ),
-                            ),
+                        IconButton(
+                          onPressed: () {
+                            deleteWishlist(docs[index].id);
+                          },
+                          icon: const Icon(
+                            Icons.favorite_border,
+                            size: 40.0,
+                            color: Colors.red,
                           ),
-                        ),
+                        )
                       ],
                     ),
                   ),
-                  IconButton(
-                    onPressed: () {
-                      deleteWishlist(docs[index].id);
-                    },
-                    icon: const Icon(
-                      Icons.favorite_border,
-                      size: 40.0,
-                      color: Colors.red,
-                    ),
-                  )
-                ],
-              ),
-            ),
-          );
+                ));
               });
         },
       ),
@@ -146,10 +155,10 @@ class _wishListState extends State<wishList> {
   }
 
   Future<void> deleteWishlist(String wishlistId) async {
-  await FirebaseFirestore.instance
-      .collection("wishlist")
-      .doc(wishlistId)
-      .delete();
+    await FirebaseFirestore.instance
+        .collection("wishlist")
+        .doc(wishlistId)
+        .delete();
   }
 
   AppBar appBar(BuildContext context) {
@@ -179,4 +188,46 @@ class _wishListState extends State<wishList> {
       ),
     );
   }
+  
+Bouquets bouquetFromSnapshot(DocumentSnapshot snapshot) {
+  return Bouquets(
+    snapshot['image'],
+    snapshot['title'],
+    snapshot['price'],
+    snapshot['rating'],
+    snapshot['description'],
+    snapshot['type'],
+  );
+}
+
+  void navigateToProductDetailPage(BuildContext context, String imgURL) {
+  // Truy vấn trong collection bouquets để lấy ra đối tượng Bouquets tương ứng
+  FirebaseFirestore.instance
+      .collection('bouquets')
+      .where('image', isEqualTo: imgURL)
+      .get()
+      .then((querySnapshot) {
+    if (querySnapshot.docs.isNotEmpty) {
+      // Tạo đối tượng Bouquets từ đối tượng DocumentSnapshot
+      final Bouquets bouquet = bouquetFromSnapshot(querySnapshot.docs[0]);
+      
+      // Truyền đối tượng Bouquets vào trang chi tiết sản phẩm
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ProductDetailPage(
+            productName: bouquet.title,
+            productPrice: bouquet.price,
+            productDescription: bouquet.description,
+            productImage: bouquet.image,
+            productRating: bouquet.rating,
+          ),
+        ),
+      );
+    } else {
+      // Xử lý khi không tìm thấy đối tượng Bouquets
+      print('Không tìm thấy sản phẩm với URL $imgURL');
+    }
+  });
+}
 }
